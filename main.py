d5972d4a698847f381a45f0e63fb08151597f9ee
omoghadasi <omoghadasi@gmail.com>
2023-06-25 14:56:14 +0330

    Refactor translation functionality to write translation results to output file and remove debug print statements
    The code changes made in this commit refactors the translation functionality to write the results to the output file, removing print statements used for debugging purposes. The `msgid` and `msgstr` lines are detected and processed correctly, and the translations are written to the output file. This improves maintainability and readability of the code.

--- main.py
+++ main.py
@@ -14,26 +14,19 @@ def translate_po_file(input_file, output_file):
         # خواندن خط به خط فایل ورودی
         for line in input_po:
             if line.startswith('msgid'):
-                print('find msgid line')
                 msgid = line.strip()[7:-1]  # برداشتن بخش متنی از msgid
-                print('msgid: ' + msgid)
                 output_po.write(line)
-                print("Line: " + line)
 
             elif line.startswith('msgstr'):
-                print('find msgstr line')
                 if msgid:
-                    print('start translating{' + msgid + ' }')
                     translation = translator.translate(str(msgid), dest='fa')
-                    print('end translating{' + msgid + ' }')
                     count_translated += 1
                     msgstr = translation.text
-                    print('msgstr: ' + msgstr)
                     output_po.write('msgstr "' + msgstr + '"\n\n')
+                    print(msgid + " ==> " + msgstr)
                     print('count: ' + str(count_translated))
             else:
                 output_po.write(line)
-                print("Line: " + line)
 
     print("\n##########################################")
     print("ترجمه با موفقیت انجام شد.")
